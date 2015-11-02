86b061c92f829ef0b2fea655da3d9f1a2c343396
Chris Coffey <cmcof273@gmail.com>
2015-11-01 21:30:08 -0500

    add delete to the binary tree set

--- Set.scala
+++ Set.scala
@@ -28,13 +28,27 @@ trait Set[S[_]]{
   def empty[A](s: S[A])(implicit o: Ordering[A]): Boolean
   def in[A](a: A, s: S[A])(implicit o: Ordering[A]): Boolean
   def insert[A](a: A, s: S[A])(implicit o: Ordering[A]): S[A]
+
+    //2 additional functions for completeness
+    def element[A](s: S[A]): Option[A]
+    def remove[A](a: A, s: S[A])(implicit o: Ordering[A]): S[A]
 }
+
+
+
+
+//1 the above implementation is incomplete. You cannot implement most set operations because there is no means
+// of getting an element out of the set or traversing the set
 
 sealed trait BinaryTree[+A]
 case class EmptyBinaryTree[+A]() extends BinaryTree[A]
 case class BinaryTreeNode[+A](l: BinaryTree[A], value: A, r: BinaryTree[A]) extends BinaryTree[A]
 
 object BinaryTree {
+    def show[A](tree: BinaryTree[A]): Unit = {
+        
+    }
+
    implicit object BinaryTreeSet extends Set[BinaryTree] {
      def empty[A](s: BinaryTree[A])(implicit o: Ordering[A]) =
       s match {
@@ -59,8 +73,40 @@ object BinaryTree {
           else if(o.gt(a, v))   in(a, r)
           else o.equiv(a, v)
       }
+
+     def element[A](s: BinaryTree[A]): Option[A] =
+        s match {
+            case EmptyBinaryTree() => None
+            case BinaryTreeNode(_, v, _) => Some(v)
+        }
+
+     def remove[A](a: A, s: BinaryTree[A])(implicit o: Ordering[A]): BinaryTree[A] = {
+        def removeMin(node: BinaryTree[A]): (BinaryTree[A], A) =
+            node match {
+                case EmptyBinaryTree() => null //not possible!
+                case BinaryTreeNode(EmptyBinaryTree(), v, r) => (r, v)
+                case BinaryTreeNode(l, v, r) =>
+                    val (t, x) = removeMin(l)
+                    (BinaryTreeNode(r, v, t), x)
+            }
+
+         s match {
+             case EmptyBinaryTree() => s
+             case BinaryTreeNode(l, v, r) if o.lt(a, v) => BinaryTreeNode(remove(a, l), v, r)
+             case BinaryTreeNode(l, v, r) if o.gt(a, v) => BinaryTreeNode(l, v, remove(a, r))
+             case BinaryTreeNode(l, v, r) if o.equiv(a, v) =>
+                 (l, r) match {
+                     case (EmptyBinaryTree(), _) => r
+                     case (_, EmptyBinaryTree()) => l
+                     case (BinaryTreeNode(_, _, _), _) =>
+                         val (t, x) = removeMin(r)
+                         BinaryTreeNode(l, x, t)
+                 }
+         }
+     }
    }
 }
+
 
 
 //sealed trait Color
