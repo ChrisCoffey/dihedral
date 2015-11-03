4ae2c11d9da158572eb0646c368ff776fd8d7afd
Chris Coffey <cmcof273@gmail.com>
2015-11-02 21:49:34 -0500

    add basic set operations for a binary tree

--- Set.scala
+++ Set.scala
@@ -1,5 +1,6 @@
 package org.ccoffey.dihedral
 
+import scala.collection.{Set => SSet}
 
 case class FunctionSet[A](rule: A => Boolean) {
   def `in:`(a: A): Boolean =
@@ -25,16 +26,60 @@ case class FunctionSet[A](rule: A => Boolean) {
 
 //what about a set typeclass?? Can I use these functions to implement all of the set behaviors above?
 trait Set[S[_]]{
-  def empty[A](s: S[A])(implicit o: Ordering[A]): Boolean
-  def in[A](a: A, s: S[A])(implicit o: Ordering[A]): Boolean
-  def insert[A](a: A, s: S[A])(implicit o: Ordering[A]): S[A]
+    def emptySet[A]: S[A]
+    def empty[A](s: S[A])(implicit o: Ordering[A]): Boolean
+    def in[A](a: A, s: S[A])(implicit o: Ordering[A]): Boolean
+    def insert[A](a: A, s: S[A])(implicit o: Ordering[A]): S[A]
 
     //2 additional functions for completeness
     def element[A](s: S[A]): Option[A]
     def remove[A](a: A, s: S[A])(implicit o: Ordering[A]): S[A]
 }
 
+//all of these operations can become far more efficient if there's a means of iterating over a set
+object Set {
+    def union[A, M[_]](m: M[A], n: M[A])(implicit s: Set[M], o: Ordering[A]): M[A] = {
+       s.element(m) match {
+           case None => n
+           case Some(a) => union(s.remove(a, m), s.insert(a, n))
+       }
+    }
 
+    //note this is left associative
+    def difference[A, M[_]](m: M[A], n: M[A])(implicit s: Set[M], o: Ordering[A]): M[A] = {
+        def go(acc: M[A], mm: M[A]): M[A] = {
+            s.element(mm) match {
+                case None => acc
+                case Some(a) =>
+                    if(s.in(a, n)) go(acc, s.remove(a, mm))
+                    else go(s.insert(a, acc), s.remove(a, mm))
+            }
+        }
+        go(s.emptySet, m)
+    }
+
+    def intersection[A, M[_]](m: M[A], n: M[A])(implicit s: Set[M], o: Ordering[A]): M[A] = {
+        def go(acc: M[A], mm: M[A]): M[A] = {
+            s.element(mm) match {
+                case None => acc
+                case Some(a) =>
+                    if(s.in(a, n)) go(s.insert(a, acc), s.remove(a, mm))
+                    else go(acc, s.remove(a, mm))
+            }
+        }
+        go(s.emptySet, m)
+    }
+
+    def symmetricDifference[A, M[_]](m: M[A], n: M[A])(implicit s: Set[M], o: Ordering[A]): M[A] =
+        union(difference(m, n), difference(n, m))
+
+    def cardinality[A, M[_]](m: M[A])(implicit s: Set[M], o: Ordering[A]): Int = {
+        def go(acc: Int, mm: M[A]): Int = {
+            s.element(mm).fold(acc){a => go(acc + 1, s.remove(a, mm))}
+        }
+        go(0, m)
+    }
+}
 
 
 //1 the above implementation is incomplete. You cannot implement most set operations because there is no means
@@ -46,10 +91,26 @@ case class BinaryTreeNode[+A](l: BinaryTree[A], value: A, r: BinaryTree[A]) exte
 
 object BinaryTree {
     def show[A](tree: BinaryTree[A]): Unit = {
-        
+        var ls: Map[Int, Seq[String]] = Map.empty[Int, Seq[String]]
+        def build(depth: Int, t: BinaryTree[A], onHead: Boolean): Unit = {
+            t match {
+                case BinaryTreeNode(l, v, r) =>
+                    val c = ls.getOrElse(depth, Seq())
+                    val nC = if(onHead) v.toString +: c else c :+ v.toString
+                    ls += (depth -> nC)
+                    build(depth + 1, l, true)
+                    build(depth + 1, r, false)
+                case EmptyBinaryTree() =>
+                    ()
+            }
+        }
+        build(0, tree, true)
+
+        println(ls.toList.sortBy(_._1).map(_._2.mkString("\t")).mkString("\n"))
     }
 
    implicit object BinaryTreeSet extends Set[BinaryTree] {
+     def emptySet[A] = EmptyBinaryTree[A]()
      def empty[A](s: BinaryTree[A])(implicit o: Ordering[A]) =
       s match {
         case EmptyBinaryTree() => true
